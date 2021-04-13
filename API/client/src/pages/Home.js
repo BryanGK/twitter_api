e5cec95050c3053aa0f3ac7778ce72c36ffdc713
BryanGK <bryangordonkrauss@gmail.com>
2021-04-13 13:13:52 -0700

    minor home.js fix

--- API/client/src/pages/Home.js
+++ API/client/src/pages/Home.js
@@ -9,7 +9,7 @@ function Home() {
                     <h1 className="display-4" id="home-header">Welcome to Happy-Tweet!</h1>
                     <p className="lead">A Twitter API showcase app.</p>
                     <hr className="my-4"></hr>
-                    <p>This full-stack web application has been built in C# with ASP.NET Core, React JS, styled using Bootstrap 4 and deployed on Azure.
+                    <p>This full-stack web application has been built in C# with ASP.NET Core, React JS, styled using Bootstrap 5 and deployed on Azure.
                     Come on in and search for tweets by keyword or username, or get random tweets that will entice the soul and excite the mind.
                     A capstone project for the Software Developer Mastermind program, where students learn by building projects, it is meant to
                     push students out of their comfort zone.
